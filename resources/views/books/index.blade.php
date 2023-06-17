0bb35bf16e8488603d0970d1743ad65e4407f9cc
Truongthong <125285601+TonySama2003@users.noreply.github.com>
2023-06-17 16:39:07 +0700

    update

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,35 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Danh sách sách</h1>
-    <form action="{{ route('books.search') }}" method="GET">
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" name="search" placeholder="Tìm kiếm sách">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="submit">Tìm kiếm</button>
+    <div class="container">
+        <h1 class="mb-4">Danh sách sách</h1>
+
+        <form action="{{ route('books.search') }}" method="GET" class="mb-4">
+            <div class="input-group">
+                <input type="text" class="form-control" name="search" placeholder="Tìm kiếm sách">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit">Tìm kiếm</button>
+                </div>
             </div>
-        </div>
-    </form>
-    @if($books->isEmpty())
-        <p>Không tìm thấy sách phù hợp.</p>
-    @else
-        <table>
-            <thead>
-            <tr>
-                <th>Tên sách</th>
-                <th>Tác giả</th>
-                <th>Năm xuất bản</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($books as $book)
-                <tr>
-                    <td>{{ $book['title'] }}</td>
-                    <td>{{ $book['author'] }}</td>
-                    <td>{{ $book['published_year'] }}</td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-    @endif
+        </form>
+
+        @if ($books->isEmpty())
+            <p class="text-center">Không tìm thấy sách phù hợp.</p>
+        @else
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>Tên sách</th>
+                        <th>Tác giả</th>
+                        <th>Năm xuất bản</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($books as $book)
+                        <tr>
+                            <td>{{ $book->title }}</td>
+                            <td>{{ $book->author }}</td>
+                            <td>{{ $book->published_year }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @endif
+    </div>
+
+    <style>
+        /* Tùy chỉnh CSS tại đây */
+        .container {
+            padding: 20px;
+        }
+
+        .mb-4 {
+            margin-bottom: 1.5rem;
+        }
+
+        .input-group {
+            margin-bottom: 1.5rem;
+        }
+
+        .table {
+            width: 100%;
+            margin-bottom: 1rem;
+            color: #212529;
+        }
+
+        .table th,
+        .table td {
+            padding: 0.75rem;
+            vertical-align: top;
+            border-top: 1px solid #dee2e6;
+        }
+
+        .table th {
+            background-color: #f8f9fa;
+        }
+
+        .table-striped tbody tr:nth-of-type(odd) {
+            background-color: rgba(0, 0, 0, 0.05);
+        }
+
+        .text-center {
+            text-align: center;
+        }
+    </style>
 @endsection
